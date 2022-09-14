470ad161ec05e3a81735a9b2086d7c864cccb68b
kubragunesy <kubragunes76@gmail.com>
2022-09-14 23:40:33 +0300

    UPDATE

---
+++ stokMVC/stokMVC/Controllers/SatisController.cs
@@ -0,0 +1,32 @@
+﻿using stokMVC.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace stokMVC.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        mvcDbStokEntities db = new mvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(satislar p)
+        {
+            db.satislar.Add(p);
+            db.SaveChanges();
+            return View("Index");
+
+        }
+    }
+}
